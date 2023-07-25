4b52ca4128f7bb41ed9dde8e239c303ff82c7ba7
Arpit Jana <arpitjana2103@gmail.com>
2023-07-25 23:02:51 +0530

    Implemented GET Request

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
-app.get('/', function (req, res) {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'SafarSathi' });
+// app.use(express.json());
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', function (req, res) {
+  return res.status(200).json({
+    status: 'success',
+    returns: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
+
+app.post('/api/');
 
 const port = 3000;
 app.listen(port, function () {
