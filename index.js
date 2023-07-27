32a156af3c50d9b2a04f062588dc85d4ca192c14
Arpit Jana <arpitjana2103@gmail.com>
2023-07-27 22:38:54 +0530

    Eslint and Prettierrc setup done

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const fs = require('fs');
+const dotenv = require('dotenv');
 const express = require('express');
+
+dotenv.config({ path: './config.env' });
 const app = express();
+
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, function () {
     console.log(`App running on port ${port}...`);
 });
+
+let x = 5;
+x = 9;
+
+console.log(x);
