b464d4d1954a7b81d2b9e4c2fe05513b90538d8e
Arpit Jana <arpitjana2103@gmail.com>
2023-07-30 19:45:08 +0530

    Successfully connected with Cloud DB

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+console.log(DB);
+
+mongoose.connect(DB).then(function () {
+    console.log('DB conndection successful');
+});
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Park Camper',
+    rating: 4.7,
+    price: 497,
+});
+
+testTour
+    .save()
+    .then(function (doc) {
+        console.log(doc);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
 const app = express();
 
 app.use(express.json());
