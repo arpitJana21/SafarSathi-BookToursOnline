a90b1def294c784df30f18d95015d3395b181a72
Arpit Jana <arpitjana2103@gmail.com>
2023-08-02 18:49:18 +0530

    Refactored API Features

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -1,4 +1,5 @@
 const { Tour } = require('../Models/tourModel');
+const { APIFeatures } = require('../utils/apiFeatures');
 
 const aliasTopTours = async function (req, res, next) {
     req.query.limit = '5';
@@ -6,57 +7,15 @@ const aliasTopTours = async function (req, res, next) {
     req.query.fields = 'name,price,ratingsAverage,summery,difficulty';
     next();
 };
+
 const getAllTours = async function (req, res) {
     try {
-        // BUILD QUERY
+        // EXECUTE QUERY
+        let features = new APIFeatures(Tour.find(), req.query);
+        features = features.filter().sort().limitFields().paginate();
+        const tours = await features.query;
 
-        // 1) FILTERING
-        let queryObj = { ...req.query };
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach(function (ele) {
-            delete queryObj[ele];
-        });
-
-        // 2) ADV. FILTERING
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, function (match) {
-            return `$${match}`;
-        });
-        queryObj = JSON.parse(queryStr);
-
-        let query = Tour.find(queryObj);
-
-        // 3) SORTING
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        // 4) FIELD LIMITING
-        if (req.query.finds) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-__v');
-        }
-
-        // 5) PAGINATION
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) throw new Error('Page Does not Exist');
-        }
-
-        // 5) EXECUTE QUERY
-        const tours = await query;
-
-        // 6) SEND RESPONSE
+        // SEND RESPONSE
         return res.status(200).json({
             status: 'success',
             results: tours.length,
---
+++ utils/apiFeatures.js
@@ -0,0 +1,59 @@
+class APIFeatures {
+    constructor(query, reqQuery) {
+        this.query = query;
+        this.reqQuery = reqQuery;
+    }
+
+    filter() {
+        // 1) FILTERING
+        let queryObj = { ...this.reqQuery };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach(function (ele) {
+            delete queryObj[ele];
+        });
+
+        // 2) ADV. FILTERING
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, function (match) {
+            return `$${match}`;
+        });
+        queryObj = JSON.parse(queryStr);
+
+        this.query = this.query.find(queryObj);
+        return this;
+    }
+
+    sort() {
+        // 3) SORTING
+        if (this.reqQuery.sort) {
+            const sortBy = this.reqQuery.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('-createdAt');
+        }
+        return this;
+    }
+
+    limitFields() {
+        // 4) FIELD LIMITING
+        if (this.reqQuery.finds) {
+            const fields = this.reqQuery.fields.split(',').join(' ');
+            this.query = this.query.select(fields);
+        } else {
+            this.query = this.query.select('-__v');
+        }
+        return this;
+    }
+
+    paginate() {
+        // 5) PAGINATION
+        const page = this.reqQuery.page * 1 || 1;
+        const limit = this.reqQuery.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        this.query = this.query.skip(skip).limit(limit);
+        return this;
+    }
+}
+
+module.exports = { APIFeatures };
