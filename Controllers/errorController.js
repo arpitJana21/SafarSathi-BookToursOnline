6d3b1fc0742462b25408e40fb7925ba10b8f07e9
Arpit Jana <arpitjana2103@gmail.com>
2023-08-08 23:41:21 +0530

    Invalid ID & Duplicate Value Error Handled (PROD)

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,3 +1,17 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = function (error) {
+    const message = `Invalid ${error.path}: ${error.value}`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateDB = function (error) {
+    //  keyValue: { name: 'The Northern Lights' }
+    const value = error.keyValue.name;
+    const message = `Duplicate field value: "${value}". Please use another value.`;
+    return new AppError(message, 400);
+};
+
 const sendErrorDev = function (err, res) {
     return res.status(err.statusCode).json({
         status: err.status,
@@ -18,7 +32,7 @@ const sendErrorProd = function (err, res) {
         // Programming or other unknown Error : don't leak the error details
     } else {
         // Log Error
-        console.error(err);
+        // console.error(err);
 
         // Send generic message
         res.status(500).json({
@@ -29,13 +43,24 @@ const sendErrorProd = function (err, res) {
 };
 
 module.exports = function (err, req, res, next) {
-    console.log(err.stack);
+    // console.log(err.stack);
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else {
-        sendErrorProd(err, res);
+        let error = { ...err };
+
+        // Trying to access Item with Invalid ID Value
+        if (err.name === 'CastError') {
+            error = handleCastErrorDB(error);
+        }
+
+        // Trying to Create Item with Duplicate Value
+        if (err.code === 11000) {
+            error = handleDuplicateDB(error);
+        }
+        sendErrorProd(error, res);
     }
 };
