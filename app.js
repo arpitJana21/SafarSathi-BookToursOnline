bebc1d7ae77eca27604d5362972b4aaddfa3e3c0
Arpit Jana <arpitjana2103@gmail.com>
2023-08-17 12:58:16 +0530

    Data Sanitization against NO-SQL queries

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const { tourRouter } = require('./Routes/toursRoutes');
 const { userRouter } = require('./Routes/userRoutes');
@@ -25,6 +26,9 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NO-SQL query injection
+app.use(mongoSanitize());
 
 // Routes Middlewares
 app.use('/api/v1/tours/', tourRouter);
