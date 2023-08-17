e527b78dd107795c3e2d3bb8a55ba35e243f7399
Arpit Jana <arpitjana2103@gmail.com>
2023-08-17 13:02:54 +0530

    Added Data Sanitization against XSS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const { tourRouter } = require('./Routes/toursRoutes');
 const { userRouter } = require('./Routes/userRoutes');
@@ -29,6 +30,9 @@ app.use(express.json({ limit: '10kb' }));
 
 // Data Sanitization against NO-SQL query injection
 app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 
 // Routes Middlewares
 app.use('/api/v1/tours/', tourRouter);
