4bde68f6a3f390763ea28b2ed6070e7293ae5ee4
Arpit Jana <arpitjana2103@gmail.com>
2023-08-25 16:00:33 +0530

    GET REVIEW by TOURID added

--- Controllers/reviewController.js
+++ Controllers/reviewController.js
@@ -2,7 +2,9 @@ const { Review } = require('../Models/reviewModel');
 const { catchAsync } = require('../utils/catchAsync');
 
 const getAllAllReviews = catchAsync(async function (req, res, next) {
-    const reviews = await Review.find();
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const reviews = await Review.find(filter);
 
     res.status(200).json({
         status: 'success',
